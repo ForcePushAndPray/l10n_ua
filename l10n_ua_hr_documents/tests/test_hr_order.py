056813acc801473aafc93a1dfbd4fa4a86c8efab
Vlad Patenko <5121488+ForcePushAndPray@users.noreply.github.com>
2026-04-29 22:38:05 +0300

    test cases fixes

--- l10n_ua_hr_documents/tests/test_hr_order.py
+++ l10n_ua_hr_documents/tests/test_hr_order.py
@@ -42,9 +42,19 @@ class TestHrOrder(TransactionCase):
         cls.leave_type = cls.env['hr.leave.type'].create({
             'name': 'Щорічна основна відпустка',
             'company_id': cls.company.id,
-            'requires_allocation': 'no',
+            'requires_allocation': 'yes',
             'leave_validation_type': 'both',
         })
+
+        cls.allocation = cls.env['hr.leave.allocation'].create({
+            'name': 'Test Leave Allocation',
+            'employee_id': cls.employee.id,
+            'holiday_status_id': cls.leave_type.id,
+            'number_of_days': 100,
+        })
+        cls.allocation.action_approve()
+        if cls.allocation.state == 'validate1':
+            cls.allocation.action_validate()
 
     def _create_order(self, order_type='hiring', **kwargs):
         vals = {
